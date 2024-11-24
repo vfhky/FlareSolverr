16acaf059e31824717b55101a3e68768494d0f1d
vfhky <vfhky@qq.com>
2024-11-24 11:22:04 +0800

    fastapi

--- src/flaresolverr.py
+++ src/flaresolverr.py
@@ -1,78 +1,36 @@
-import json
 import logging
 import os
 import sys
 
-import certifi
-from bottle import run, response, Bottle, request, ServerAdapter
+from fastapi import FastAPI, HTTPException
+import uvicorn
 
-from bottle_plugins.error_plugin import error_plugin
-from bottle_plugins.logger_plugin import logger_plugin
-from bottle_plugins import prometheus_plugin
-from dtos import V1RequestBase
 import flaresolverr_service
 import utils
+from dtos import V1RequestBase
+
+app = FastAPI()
 
 
-class JSONErrorBottle(Bottle):
-    """
-    Handle 404 errors
-    """
-    def default_error_handler(self, res):
-        response.content_type = 'application/json'
-        return json.dumps(dict(error=res.body, status_code=res.status_code))
 
-
-app = JSONErrorBottle()
-
-
-@app.route('/')
-def index():
-    """
-    Show welcome message
-    """
-    res = flaresolverr_service.index_endpoint()
-    return utils.object_to_dict(res)
-
-
-@app.route('/health')
-def health():
-    """
-    Healthcheck endpoint.
-    This endpoint is special because it doesn't print traces
-    """
-    res = flaresolverr_service.health_endpoint()
-    return utils.object_to_dict(res)
-
-
-@app.post('/v1')
-def controller_v1():
+@app.post("/v1")
+async def controller_v1(req: V1RequestBase):
     """
     Controller v1
     """
-    req = V1RequestBase(request.json)
+    print(f"req={req}")
     res = flaresolverr_service.controller_v1_endpoint(req)
     if res.__error_500__:
-        response.status = 500
-    return utils.object_to_dict(res)
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
+    return res
 
 
+# Main entry point
 if __name__ == "__main__":
     # check python version
     if sys.version_info < (3, 9):
         raise Exception("The Python version is less than 3.9, a version equal to or higher is required.")
-
-    # fix for HEADLESS=false in Windows binary
-    # https://stackoverflow.com/a/27694505
-    if os.name == 'nt':
-        import multiprocessing
-        multiprocessing.freeze_support()
-
-    # fix ssl certificates for compiled binaries
-    # https://github.com/pyinstaller/pyinstaller/issues/7229
-    # https://stackoverflow.com/questions/55736855/how-to-change-the-cafile-argument-in-the-ssl-module-in-python3
-    os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
-    os.environ["SSL_CERT_FILE"] = certifi.where()
 
     # validate configuration
     log_level = os.environ.get('LOG_LEVEL', 'info').upper()
@@ -93,6 +51,7 @@ if __name__ == "__main__":
             logging.StreamHandler(sys.stdout)
         ]
     )
+    
     # disable warning traces from urllib3
     logging.getLogger('urllib3').setLevel(logging.ERROR)
     logging.getLogger('selenium.webdriver.remote.remote_connection').setLevel(logging.WARNING)
@@ -101,25 +60,13 @@ if __name__ == "__main__":
     logging.info(f'FlareSolverr {utils.get_flaresolverr_version()}')
     logging.debug('Debug log enabled')
 
-    # Get current OS for global variable
+    # Get and set current OS for global variable
     utils.get_current_platform()
 
     # test browser installation
     flaresolverr_service.test_browser_installation()
 
-    # start bootle plugins
-    # plugin order is important
-    app.install(logger_plugin)
-    app.install(error_plugin)
-    prometheus_plugin.setup()
-    app.install(prometheus_plugin.prometheus_plugin)
+    webdriver_data = utils.get_webdriver_data_path()
+    utils.remove_all_subfolders(webdriver_data)
 
-    # start webserver
-    # default server 'wsgiref' does not support concurrent requests
-    # https://github.com/FlareSolverr/FlareSolverr/issues/680
-    # https://github.com/Pylons/waitress/issues/31
-    class WaitressServerPoll(ServerAdapter):
-        def run(self, handler):
-            from waitress import serve
-            serve(handler, host=self.host, port=self.port, asyncore_use_poll=True)
-    run(app, host=server_host, port=server_port, quiet=True, server=WaitressServerPoll)
+    uvicorn.run(app, host=server_host, port=server_port)
